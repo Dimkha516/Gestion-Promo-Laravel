f6c7460cff244255d2bc9fab478e0d613355c304
Dimkha <dimkha516@gmail.com>
2024-09-25 09:32:24 +0000

    authorizeCredentials

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,7 +13,7 @@ use Illuminate\Routing\Controller as BaseController;
  *      description="Documentation for the API",
  *      @OA\Contact(
  *          email="dimkha516@gmail.com"
- *      ),
+ *      ),()
  *      @OA\License(
  *          name="Apache 2.0",
  *          url="https://gestion-promo-laravel.onrender.com"
